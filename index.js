42dc8eb2dd27fbe5dc283c70c923658b78a7e6a8
gustavooeloi <gustavooeloi@gmail.com>
2024-03-12 08:34:37 -0300

    delete by id

--- index.js
+++ index.js
@@ -61,7 +61,28 @@ app.put("/api/product/:id", async (req, res) => {
     const product = await Product.findByIdAndUpdate(id, req.body, {
       new: true,
     });
+
+    if (!product) {
+      return res.status(404).json({ status: "Product not found!" });
+    }
+
     res.status(200).json({ status: "Product updated!", product: product });
+  } catch (err) {
+    res.status(500).json({ status: err.message });
+  }
+});
+
+// Delete by ID
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ status: "Product not found!" });
+    }
+
+    res.status(200).json({ status: "Product deleted!" });
   } catch (err) {
     res.status(500).json({ status: err.message });
   }
